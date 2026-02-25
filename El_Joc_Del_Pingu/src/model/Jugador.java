d8a22ec506499cf32350d504913bf68d3fcdd610
pereboloix637 <pereboloix637@alumnes.ilerna.com>
2026-02-25 16:00:54 +0100

    Cambiat el jugador.java
    Ara te constructors i atributs

--- El_Joc_Del_Pingu/src/model/Jugador.java
+++ El_Joc_Del_Pingu/src/model/Jugador.java
@@ -1,5 +1,27 @@
 package model;
 
 public class Jugador {
-public String nickname;
+	/// ATRIBUTS
+	String nickname;
+	int posicio;
+	String color;
+	
+	/// CONSTRUCTORS
+	// AMB TOTS ELS PARAMETRES
+	public Jugador (String nickname, int posicio, String color) {
+	this.nickname = nickname;
+	this.posicio = posicio;
+	this.color = color;
+	}
+	
+	// AUTOMIZAT AMB NICKNAME
+	public Jugador (String nickname) {
+		this.nickname = nickname;
+		this.posicio = 0;
+		this.color = color;
+	
+	/// METODES
+	// PROXIMAMENT
+	
+	
 }
