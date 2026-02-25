55d264f04a14521c0ae481293f450bfe0d61abde
Ivanks3k <korobovasablina@alumnes.ilerna.com>
2026-02-25 16:50:56 +0100

    Creación del codigo de "Inventari"
    Creación del codigo de "Inventari"

--- El_Joc_Del_Pingu/src/model/items/Inventari.java
+++ El_Joc_Del_Pingu/src/model/items/Inventari.java
@@ -1,5 +1,24 @@
 package model.items;
 
+import java.util.ArrayList;
+
+
 public class Inventari {
 
+    private ArrayList<Item> llista;
+
+    // Constructor
+    public Inventari() {
+        llista = new ArrayList<>();
+    }
+
+    // Getter
+    public ArrayList<Item> getLlista() {
+        return llista;
+    }
+
+    // Setter
+    public void setLlista(ArrayList<Item> llista) {
+        this.llista = llista;
+    }
 }
