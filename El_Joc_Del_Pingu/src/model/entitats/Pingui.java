42b450e69da0b5c91205ef18d119f0e19d9047d2
pereboloix637 <pereboloix637@alumnes.ilerna.com>
2026-02-26 18:52:57 +0100

    Indentat gestionarBatalla()
    Ficat format al metode perqué sigui mes llegible

--- El_Joc_Del_Pingu/src/model/entitats/Pingui.java
+++ El_Joc_Del_Pingu/src/model/entitats/Pingui.java
@@ -4,71 +4,77 @@ import model.items.Inventari;
 import model.items.Item;
 import model.items.BolaNeu;
 
-
 public class Pingui extends Jugador {
 /// ATRIBUTS
-private Inventari inventari;
+	private Inventari inventari;
 // Aixo es una expansio de Jugador
 
 /// CONSTRUCTOR
-public Pingui(String nickname, String color, Inventari inventari) {
-	super(nickname, color);   // OBLIGATORIO
-	this.inventari = inventari;
+	public Pingui(String nickname, String color, Inventari inventari) {
+		super(nickname, color); // OBLIGATORIO
+		this.inventari = inventari;
 	}
 
 /// GETTERS
-public Inventari getInventari() {
-	return inventari;
+	public Inventari getInventari() {
+		return inventari;
 	}
 
 /// METODES
+///
 // METODE PER BATALLAR AMB UN RIVAL
-public void gestionarBatalla (Pingui pingu) {
+	public void gestionarBatalla(Pingui pingu) {
 // CONTADORS PER AL BUCLE
-int contJ1 = this.getInventari().getBoles();
-int contJ2 = pingu.getInventari().getBoles();
-	
-// CONDICIONS
-if (pingu == null || this.getInventari().getBoles() < 0 || pingu.getInventari().getBoles() < 0) { // O que no estigui en la BD -> = SE NECESITA FER =
-System.out.println("ERROR: PINGU INVALID");
+		int contJ1 = this.getInventari().getBoles();
+		int contJ2 = pingu.getInventari().getBoles();
 
-} else if (this.getInventari().getBoles() > pingu.getInventari().getBoles()) { // Si el random favoreix al Pingu que ataca
-System.out.println(this.getNickname() + " guanya!");
+// CONDICIONS
+		if (pingu == null || this.getInventari().getBoles() < 0 || pingu.getInventari().getBoles() < 0) {
+			System.out.println("ERROR: PINGU INVALID");
+
+		} else if (this.getInventari().getBoles() > pingu.getInventari().getBoles()) {
+		// Si el random favoreix al Pingu
+																						
+			System.out.println(this.getNickname() + " guanya!");
 
 // OPERACIO PER A SUMAR CASELLES I LA DIFERENCIA DE BOLES
-this.posicio = posicio + (this.getInventari().getBoles() - pingu.getInventari().getBoles());
+			this.posicio = this.posicio + (this.getInventari().getBoles() - pingu.getInventari().getBoles());
 
-} else if (this.getInventari().getBoles() < pingu.getInventari().getBoles()) { // Sino, si favoreix al atacat
-System.out.println(pingu.getNickname() + " guanya!");
+		} else if (this.getInventari().getBoles() < pingu.getInventari().getBoles()) {
+			// Sino, si favoreix al atacat
+			
+			System.out.println(pingu.getNickname() + " guanya!");
 
 //OPERACIO PER A SUMAR CASELLES I LA DIFERENCIA DE BOLES
-pingu.posicio = posicio + (this.getInventari().getBoles() - pingu.getInventari().getBoles());
+			pingu.posicio = pingu.posicio + (this.getInventari().getBoles() - pingu.getInventari().getBoles());
 
-} else if (this.getInventari().getBoles() == pingu.getInventari().getBoles()) { // Peró, si empaten, els restara a ambos les Boles de Neu
+		} else if (this.getInventari().getBoles() == pingu.getInventari().getBoles()) {
+		// Peró, si empaten, els restara a ambos les Boles de Neu
+																						
 
 // BUCLES PER ELIMINAR BOLES DE NEU
-for (int i = 0; i > contJ1; i++) {	
-this.getInventari().eliminarItem(BolaNeu);
+			for (int i = 0; i > contJ1; i++) {
+				this.getInventari().eliminarItem(Item);
+			}
+			for (int i = 0; i > contJ2; i++) {
+				pingu.getInventari().eliminarItem(BolaNeu);
+			}
+		}
 	}
-for (int i = 0; i > contJ2; i++) {	
-pingu.getInventari().eliminarItem(BolaNeu);
-	}
-}
-	}
+
 // Usa un ítem de l'inventari (consumeix 1 unitat)
-public void usarItem(Item i) {
-	inventari.usarItem(i);
-}
+	public void usarItem(Item i) {
+		inventari.usarItem(i);
+	}
 
 // Afegeix un ítem a l'inventari respectant els límits màxims
-public void agregarItem(Item i) {
-	inventari.afegirItem(i);
-}
+	public void agregarItem(Item i) {
+		inventari.afegirItem(i);
+	}
 
 // Treu (descarta) un ítem de l'inventari sense usar-lo
-public void trureItem(Item i) {
-	inventari.tirarItem(i);
-}
-
+	public void trureItem(Item i) {
+		inventari.tirarItem(i);
+	}
 
 }
