349ef5c601fe72f2611557e6c396bdf619a004ec
Cesc Bellet <cescbelletboneu@alumnes.ilerna.com>
2026-02-26 18:49:26 +0100

    models caselles indentats
    models caselles indentats

--- El_Joc_Del_Pingu/src/model/caselles/Casella.java
+++ El_Joc_Del_Pingu/src/model/caselles/Casella.java
@@ -6,18 +6,23 @@ import model.entitats.Jugador;
 /** Casella abstracta del taulell. Cada subclasse defineix el seu efecte. */
 public abstract class Casella {
 
-    // Atributs
-    private int posicio;
+	// Atributs
+	private int posicio;
 
-    // Constructor
-    public Casella(int posicio) {
-        this.posicio = posicio;
-    }
+	// Constructor
+	public Casella(int posicio) {
+		this.posicio = posicio;
+	}
 
-    // Getters i Setters
-    public int  getPosicio() { return posicio; }
-    public void setPosicio(int posicio) { this.posicio = posicio; }
+	// Getters i Setters
+	public int getPosicio() {
+		return posicio;
+	}
 
-    // Mètode abstracte – cada subclasse implementa la seva acció
-    public abstract void realitzarAccio(Partida partida, Jugador jugador);
+	public void setPosicio(int posicio) {
+		this.posicio = posicio;
+	}
+
+	// Mètode abstracte – cada subclasse implementa la seva acció
+	public abstract void realitzarAccio(Partida partida, Jugador jugador);
 }
