4f2dfbf10e620479e808e3c205659ebe5e9c552d
pereboloix637 <pereboloix637@alumnes.ilerna.com>
2026-02-26 15:33:21 +0100

    Començat Taulell.java

--- El_Joc_Del_Pingu/src/model/core/Taulell.java
+++ El_Joc_Del_Pingu/src/model/core/Taulell.java
@@ -1,5 +1,22 @@
 package model.core;
 
+import java.util.ArrayList;
+
 public class Taulell {
 
+	private ArrayList<Casella> taulell;
+
+    // Constructor
+    public Inventari() {
+        llista = new ArrayList<>();
+    }
+
+    // Getter
+    public ArrayList<Item> getLlista() {
+        return llista;
+    }
+
+    // Setter
+    public void setLlista(ArrayList<Item> llista) {
+        this.llista = llista;
 }
