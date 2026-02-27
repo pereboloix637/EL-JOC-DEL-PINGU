172289f2a0d5f4475fd3b724a60cd0ede3cd048a
Cesc Bellet <cescbelletboneu@alumnes.ilerna.com>
2026-02-27 16:48:56 +0100

    Gestor taulell fet
    Creats els dos metodes del gestor del taulell.

--- El_Joc_Del_Pingu/src/controlador/GestorTaulell.java
+++ El_Joc_Del_Pingu/src/controlador/GestorTaulell.java
@@ -1,5 +1,34 @@
 package controlador;
 
+import java.util.ArrayList;
+import model.caselles.Casella;
+import model.core.Partida;
+import model.core.Taulell;
+import model.entitats.Jugador;
+import model.entitats.Pingui;
+
 public class GestorTaulell {
 
+	/**
+	 * Executa l'acció de la casella sobre el pingüí.
+	 */
+	public void executarCasella(Partida partida, Pingui pingui, Casella casella) {
+		casella.realitzarAccio(partida, pingui);
+	}
+
+	/**
+	 * Comprova si la partida ha finalitzat i estableix el guanyador.
+	 */
+	public void comprovarFiTorn(Partida partida) {
+		Jugador jugadorActual = partida.getJugadorActual();
+		Taulell taulell = partida.getTaulell();
+
+		int posicio = jugadorActual.getPosicio();
+		int totalCaselles = taulell.getCaselles().size();
+
+		if (posicio >= totalCaselles - 1) {
+			partida.setFinalitzada(true);
+			partida.setGuanyador(jugadorActual);
+		}
+	}
 }
