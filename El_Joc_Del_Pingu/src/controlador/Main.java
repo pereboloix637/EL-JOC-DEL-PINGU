e1cb15f13eee28da0514b6a904a84fcd8c05678d
Cesc Bellet <cescbelletboneu@alumnes.ilerna.com>
2026-02-27 15:56:00 +0100

    bbdd integrada
    La bbdd esta integrada y se ha añadido la libreria al proyecto y el controlador de la db, aparte de en el main el codigo para establecer la conexión.

--- El_Joc_Del_Pingu/src/controlador/Main.java
+++ El_Joc_Del_Pingu/src/controlador/Main.java
@@ -1,5 +1,25 @@
 package controlador;
 
-public class Main {
+import java.sql.Connection;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        // Conectamos usando el método del archivo BBDD.java
+        Connection con = BBDD.conectarBaseDatos(scan);
+        
+        // Comprobamos si la conexión ha funcionado
+        if (con != null) {
+            System.out.println("¡Conexión exitosa!");
+        } else {
+            System.out.println("No se ha podido conectar.");
+        }
+        
+        // Cerramos la conexión
+        BBDD.cerrar(con);
+        scan.close();
+    }
 }
