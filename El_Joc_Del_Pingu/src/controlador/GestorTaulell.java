2b298f7b19b62ffddb74aa7be947a38e98d1618a
Cesc Bellet <cescbelletboneu@alumnes.ilerna.com>
2026-02-27 19:49:08 +0100

    Sistema de generació de taulell
    Sistema de generació de taulell fet, per a poder guardar de forma comoda el taulell, he fet un metode per a generar un seed aleatori amb validacions per crear taulells justos i a sobre he fer un metode per a decodificar les seeds per a generar el taulell. No esta del tot complert pero esta casi.

--- El_Joc_Del_Pingu/src/controlador/GestorTaulell.java
+++ El_Joc_Del_Pingu/src/controlador/GestorTaulell.java
@@ -1,6 +1,8 @@
 package controlador;
 
 import java.util.ArrayList;
+import java.util.Random;
+
 import model.caselles.Casella;
 import model.core.Partida;
 import model.core.Taulell;
@@ -30,5 +32,133 @@ public class GestorTaulell {
 			partida.setFinalitzada(true);
 			partida.setGuanyador(jugadorActual);
 		}
+	}
+
+	/**
+	 * Genera un taulell a partir d'un seed de 50 caràcters (funciona tant si l'usuari passa un String
+	 * "0123...45" o qualsevol altra representació numèrica com a text).
+	 * 0=NORMAL, 1=OS, 2=TRINEU, 3=AIGUERO(Forat), 4=ESDEVENIMENT(Event), 5=TRENCADIS
+	 *
+	 * @param seed Un string amb 50 dígits, on cadascun representa el tipus de casella.
+	 * @return El taulell instanciat amb les caselles corresponents.
+	 */
+	public Taulell generarTaulell(String seed) {
+		ArrayList<Casella> caselles = new ArrayList<>();
+
+		// Si el seed no és vàlid, creem un per defecte format només per normals
+		if (seed == null || seed.length() != 50) {
+			System.out.println("Error: El seed ha de tenir exactament 50 caràcters. Es generarà un taulell normal per defecte.");
+			for (int i = 0; i < 50; i++) {
+				caselles.add(new model.caselles.Normal(i));
+			}
+			return new Taulell(caselles);
+		}
+
+		String[] nomsEvents = new String[]{"Peix", "Boles de Neu", "Dau Ràpid", "Dau Lent"};
+
+		for (int i = 0; i < 50; i++) {
+			char type = seed.charAt(i);
+			Casella casella;
+
+			switch (type) {
+				case '1':
+					casella = new model.caselles.Os(i);
+					break;
+				case '2':
+					casella = new model.caselles.Trineu(i);
+					break;
+				case '3':
+					casella = new model.caselles.Forat(i);
+					break;
+				case '4':
+					casella = new model.caselles.Event(i, nomsEvents);
+					break;
+				case '5':
+					casella = new model.caselles.Trencadis(i);
+					break;
+				case '0':
+				default:
+					casella = new model.caselles.Normal(i);
+					break;
+			}
+			caselles.add(casella);
+		}
+
+		return new Taulell(caselles);
+	}
+
+	/**
+	 * Genera un seed aleatori de 50 caràcters per al taulell, complint amb les següents regles:
+	 * - Les 4 primeres caselles i les 2 últimes són normals (0).
+	 * - Limita la quantitat de caselles especials per evitar repetició excessiva (màxim 5 de cada tipus).
+	 *
+	 * @return Un String de 50 caràcters per utilitzar amb generarTaulell(seed).
+	 */
+	public String generarSeedAleatori() {
+		StringBuilder seed = new StringBuilder(50);
+		Random random = new Random();
+
+		// Comptadors màxims per evitar que hi hagi moltes d'un mateix tipus repetides
+		int maxOs = 5;
+		int maxTrineu = 5;
+		int maxForat = 5;
+		int maxEvent = 5;
+		int maxTrencadis = 5;
+
+		for (int i = 0; i < 50; i++) {
+			if (i < 4 || i >= 48) {
+				// Les primeres 4 i les 2 últimes obligatòriament són normals
+				seed.append('0');
+			} else {
+				boolean afegit = false;
+				while (!afegit) {
+					int type = random.nextInt(6); // de 0 a 5
+
+					switch (type) {
+						case 0:
+							seed.append('0');
+							afegit = true;
+							break;
+						case 1:
+							if (maxOs > 0) {
+								seed.append('1');
+								maxOs--;
+								afegit = true;
+							}
+							break;
+						case 2:
+							if (maxTrineu > 0) {
+								seed.append('2');
+								maxTrineu--;
+								afegit = true;
+							}
+							break;
+						case 3:
+							if (maxForat > 0) {
+								seed.append('3');
+								maxForat--;
+								afegit = true;
+							}
+							break;
+						case 4:
+							if (maxEvent > 0) {
+								seed.append('4');
+								maxEvent--;
+								afegit = true;
+							}
+							break;
+						case 5:
+							if (maxTrencadis > 0) {
+								seed.append('5');
+								maxTrencadis--;
+								afegit = true;
+							}
+							break;
+					}
+				}
+			}
+		}
+
+		return seed.toString();
 	}
 }
