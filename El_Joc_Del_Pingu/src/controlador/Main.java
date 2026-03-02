dca09895b3aa54cba41872ce59a0afed6c19e8f7
pereboloix637 <pereboloix637@alumnes.ilerna.com>
2026-03-02 16:21:49 +0100

    Moltes conexions al MAIN

--- El_Joc_Del_Pingu/src/controlador/Main.java
+++ El_Joc_Del_Pingu/src/controlador/Main.java
@@ -2,7 +2,15 @@ package controlador;
 
 import java.sql.Connection;
 import java.util.Scanner;
+import model.items.Inventari;
+import model.items.Item;
+import model.entitats.Pingui;
+import model.items.BolaNeu;
 
 public class Main {
+	BolaNeu BN = new BolaNeu ("BolaNeu", 2, 3);
+	
+	Inventari inv = new Inventari();
 
+	Pingui pingu = new Pingui("PereBC", "azul", inv);
 }
