d73e76c175383b021b542cdab25813fba439fdce
Cesc Bellet <cescbelletboneu@alumnes.ilerna.com>
2026-03-05 17:04:35 +0100

    main vaciado
    main vaciado

--- El_Joc_Del_Pingu/src/controlador/Main.java
+++ El_Joc_Del_Pingu/src/controlador/Main.java
@@ -2,17 +2,10 @@ package controlador;
 
 public class Main {
 	public static void main(String[] args) {
-		
+
 		GestorTaulell Gtaula = new GestorTaulell();
-		
+
 		System.out.println(Gtaula.generarSeedAleatori());
-		
-		     
-		       
-		    
-		       
-			}
-		}
 
-
-
+	}
+}
