8222abba5d274cc3142b765ba2102ce418d745fa
Cesc Bellet <cescbelletboneu@alumnes.ilerna.com>
2026-03-05 17:14:06 +0100

    main buidat
    main buidat

--- El_Joc_Del_Pingu/src/controlador/Main.java
+++ El_Joc_Del_Pingu/src/controlador/Main.java
@@ -3,9 +3,5 @@ package controlador;
 public class Main {
 	public static void main(String[] args) {
 
-		GestorTaulell Gtaula = new GestorTaulell();
-
-		System.out.println(Gtaula.generarSeedAleatori());
-
 	}
 }
